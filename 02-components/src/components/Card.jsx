018cb9bf7d1173d8a24e9283011fc117a08927c1
shiva7465k@gmail.com <shiva7465k@gmail.com>
2026-08-24 17:36:23 +0530

    Add 02-components project

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,8 @@
+function Card() {
+    return <div className="card">
+        <h1>Shiva</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. repudiandae!</p>
+    </div>
+
+}
+export default Card;
